1a649b375b0ad892c07deeddd8e9d369b77928b8
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-17 16:13:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function happyHolidayTo(holiday, name){
   console.log('Happy ${holiday}, ${name}!')
   return "Happy Independence Day, you!"
 }
-
 function holidayCountdown(holiday, days){
 console.log("It's ${days} days until ${holiday}!")
 return "It's 20 days until Mother's Day!"
